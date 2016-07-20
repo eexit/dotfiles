b5e37dafeac4f8064560b836d2261fc70501a86c
Joris Berthelot <jberthelot@profilsoft.com>
2016-07-20 22:31:49 +0200

    Add my aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,27 @@
 alias reload!='. ~/.zshrc'
+alias c='clear'
+alias less='less --quiet'
+alias df='df -h'
+alias du='du -hs'
 
-alias cls='clear' # Good 'ol Clear Screen command
+# Fast directory change
+alias -g ..='cd ..'
+alias -g ...='cd ../..'
+alias -g ....='cd ../../..'
+alias -g .....='cd ../../../..'
+alias -g ......='cd ../../../../..'
+alias -g .......='cd ../../../../../..'
+
+# List directory contents
+alias l='ls -lahG'
+alias ll='ls -lhG'
+alias la='ls -lAhG'
+
+# Always ask for confirmation before overwriting
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+
+alias pbcopy='tr -d "\n" | /usr/bin/pbcopy'
+alias mailcatcher='mailcatcher --ip=0.0.0.0'
+alias phpdebug='PHP_IDE_CONFIG="serverName=localhost" XDEBUG_CONFIG="idekey=PHPSTORM" php -d xdebug.remote_host=localhost -d xdebug.remote_connect_back=0'
