23b215c129ff389c0ba22828021de154bc054f8b
Joris Berthelot <jberthelot@profilsoft.com>
2016-07-25 17:54:14 +0200

    More OSX defaults

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -10,11 +10,54 @@
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
+# Locale
+defaults write -g AppleLanguages -array "en" "fr"
+defaults write -g AppleLocale -string "en_FR@currency=EUR"
+defaults write -g AppleMeasurementUnits -string "Centimeters"
+defaults write -g AppleMetricUnits -bool true
+
+# 12-Hour Time
+defaults write -g AppleICUForce12HourTime -bool true
+
+# Small Sidebar icon size
+defaults write -g NSTableViewDefaultSizeMode -int 1
+
+# Disable auto-correct
+defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Allow to Quit the Finder
+defaults write com.apple.finder QuitMenuItem -bool true
+
+# Show Finder status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# No translucent menu bar
+defaults write -g AppleEnableMenuBarTransparency -bool false
+
+# Ask for password after 60 seconds
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 60
+
+# My Dock size
+defaults write com.apple.dock tilesize -int 33
+
+# Show indicator lights for open applications in the Dock
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# Don't hide the Dock
+defaults write com.apple.dock autohide -bool false
+
+# Set natural scroll direction
+defaults write -g com.apple.swipescrolldirection -bool true
+
 # Use AirDrop over every interface. srsly this should be a default.
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
 # Always open everything in Finder's column view. This is important.
-defaults write com.apple.Finder FXPreferredViewStyle clmv
+defaults write com.apple.Finder FXPreferredViewStyle -string "clmv"
 
 # Show the ~/Library folder.
 chflags nohidden ~/Library
@@ -25,9 +68,36 @@ defaults write -g com.apple.mouse.scaling -float 0.6875
 # Trackpad speed
 defaults write -g com.apple.trackpad.scaling -float 1
 
-# Tap/gestures settings
+# Enable text selection in quick look windows
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Tap to click
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+
+# Tap with two fingers to emulate right click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+
+# Don't drag and lock
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad DragLock -int 0
+
+# Smart zoom, double-tap with two fingers
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadTwoFingerDoubleTapGesture -int 1
+
+# Disable notification center gesture
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadTwoFingerFromRightEdgeSwipeGesture -int 0
+
+# Zoom in or out
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadPinch -int 1
+
+# Trackpad rotate
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRotate -int 1
+
+# Disable three finger drag
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool false
+
+# Disable three finger tap (look up)
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerTapGesture -int 0
+
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Dragging -int 0
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 0
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFiveFingerPinchGesture -int 2
@@ -37,24 +107,39 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFi
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadHandResting -bool true
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadHorizScroll -int 1
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadMomentumScroll -bool true
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadPinch -int 1
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRotate -int 1
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadScroll -bool true
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool false
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 0
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerTapGesture -int 0
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerVertSwipeGesture -int 0
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadTwoFingerDoubleTapGesture -int 1
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadTwoFingerFromRightEdgeSwipeGesture -int 0
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad USBMouseStopsTrackpad -int 0
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 0
+
+defaults write com.apple.dock showMissionControlGestureEnabled -bool true
+defaults write com.apple.dock showAppExposeGestureEnabled -bool true
+defaults write com.apple.dock showDesktopGestureEnabled -bool true
+defaults write com.apple.dock showLaunchpadGestureEnabled -bool true
 
 # Set a really fast key repeat.
 defaults write -g KeyRepeat -int 0
 
-# Set the Finder prefs for showing a few different volumes on the Desktop.
+# Expand save panel by default
+defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
+
+# Expand print panel by default
+defaults write -g PMPrintingExpandedStateForPrint -bool true
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Disanle swipe between pages with two fingers
+defaults write -g AppleEnableSwipeNavigateWithScrolls -bool false
+
+# Show icons for hard drives, servers, and removable media on the desktop
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
 # No screensaver!
 defaults -currentHost write com.apple.screensaver idleTime 0
