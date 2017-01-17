b3b5ac8faa0c48e57fdef5b2922750c5bd4895d9
Joris Berthelot <jberthelot@profilsoft.com>
2017-01-17 18:00:21 +0100

    Update .gitconfig

--- git/completion.zsh
+++ git/completion.zsh
@@ -1,11 +1,11 @@
 # Uses git's autocompletion for inner commands. Assumes an install of git's
 # bash `git-completion` script at $completion below (this is where Homebrew
 # tosses it, at least).
-completion='$(brew --prefix)/share/zsh/site-functions/_git'
+completion=`brew --prefix`/share/zsh/site-functions/_git
 
 if test -f $completion
 then
-  source $completion
+    #source $completion
 fi
 
 # Removes Git info file from autocompletion -> takes away the slowness
