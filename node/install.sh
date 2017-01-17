bf89f25695e70ffac0d58ce296fee0c19aff0222
Joris Berthelot <jberthelot@profilsoft.com>
2017-01-17 18:08:58 +0100

    Add tldr and emoj

--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,14 @@
-if test ! $(which spoof)
+#!/bin/zsh
+set -e
+
+packages=(spoof tldr emoj)
+
+for cmd in "${packages[@]}"
+do
+    (( $+commands[$cmd] )) && npm upgrade -g $cmd || npm install -g $cmd
+done
+
+if test ! -f "$ZSH/functions/_npm"
 then
-  sudo npm install spoof -g
+    npm completion > "$ZSH/functions/_npm"
 fi
