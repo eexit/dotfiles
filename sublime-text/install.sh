b747b808d712bb97749364655bcf4efbc8107587
Joris Berthelot <admin@eexit.net>
2017-09-22 16:37:47 +0200

    Fix sublime-text

--- sublime-text/install.sh
+++ sublime-text/install.sh
@@ -1,14 +1,18 @@
 #!/bin/zsh
 set -e
 
-installdir="~/Library/Application Support/Sublime Text 3"
+cwd=$(pwd)
+s3=~/Library/Application\ Support/Sublime\ Text\ 3
 
 if (( $+commands[subl] ))
 then
-    pckg="$installdir/Installed Packages/Package Control.sublime-package"
-    if test -f $pckg
+    cd $s3
+    pckg=./Installed\ Packages/Package\ Control.sublime-package
+    if [ ! -e $pckg ]
     then
-        curl "https://packagecontrol.io/Package%20Control.sublime-package" -o $pckg
+        curl https://packagecontrol.io/Package%20Control.sublime-package -o $pckg
     fi
-    cp -f User/* "$installdir/Packages/User/"
+    rm -rf ./Packages/User
+    ln -s $ZSH/sublime-text/User ./Packages/
+    cd $cwd
 fi
