7a7551a025696eaf3132fe774ca499839f6152b4
Joris Berthelot <admin@eexit.net>
2020-06-11 22:33:07 +0200

    Bye-bye Spectacle, hello Rectangle

---
+++ rectangle/install.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+set -e
+ln -s $ZSH/rectangle/com.knollsoft.Rectangle.plist ~/Library/Preferences/com.knollsoft.Rectangle.plist
--- spectacle/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-set -e
-
-ln -s $ZSH/spectacle/Shortcuts.json ~/Library/Application\ Support/Spectacle/Shortcuts.json
