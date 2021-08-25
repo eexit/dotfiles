86f29991cc1f8c1b0cfed94f3a5f216caf25e285
Joris Berthelot <joris.berthelot@careerbuilder.com>
2021-08-25 22:57:05 +0200

    Add alfred prefs

---
+++ alfred/install.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+set -e
+
+dirs=(appearence features)
+
+for dir in "${dirs[@]}"
+do
+    rm -rf ~/Library/Application\ Support/Alfred/Alfred.alfredpreferences/preferences/$dir || true
+    ln -s $ZSH/alfred/appearence ~/Library/Application\ Support/Alfred/Alfred.alfredpreferences/preferences/$dir
+done
+
+localdir=$(/bin/ls -1 ~/Library/Application\ Support/Alfred/Alfred.alfredpreferences/preferences/local)
+
+rm -rf ~/Library/Application\ Support/Alfred/Alfred.alfredpreferences/preferences/local/$localdir/*
+
+dirs=(appearence features hotkey)
+
+for dir in "${dirs[@]}"
+do
+    ln -s $ZSH/alfred/local/appearence ~/Library/Application\ Support/Alfred/Alfred.alfredpreferences/preferences/local/$localdir/$dir
+done
