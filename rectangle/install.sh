d20ae8825e8199dfb01d1b2943aaac8aa39f7b91
Joris Berthelot <jberthelot@rosaly.com>
2021-12-17 11:39:13 +0100

    Update rectangle config

--- rectangle/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-set -e
-rm -rf ~/Library/Preferences/com.knollsoft.Rectangle.plist || true
-ln -s $ZSH/rectangle/com.knollsoft.Rectangle.plist ~/Library/Preferences/com.knollsoft.Rectangle.plist
